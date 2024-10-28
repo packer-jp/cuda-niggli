64a37502eddff716c63afb1fd30ca3b759410b81
packer-jp <tone0805develop@gmail.com>
2024-10-28 20:03:20 +0900

    呼び出しコード修正

--- src/torch_niggli/torch_niggli.cu
+++ src/torch_niggli/torch_niggli.cu
@@ -8,7 +8,9 @@
 torch::Tensor niggliReduceTorch(torch::Tensor cell, float tol = 1e-5, int num_iterations = 1000)
 {
     cell = cell.contiguous();
+    cudaSetDevice(cell.device().index());
     TORCH_CHECK(cell.device().is_cuda(), "Cell tensor must be on CUDA device");
+    TORCH_CHECK(cell.size(1) == 3 && cell.size(2) == 3, "Cell tensor must have shape [N, 3, 3]");
 
     int num_cells = cell.size(0);
 
@@ -16,7 +18,11 @@ torch::Tensor niggliReduceTorch(torch::Tensor cell, float tol = 1e-5, int num_it
 
     cudaDeviceSynchronize();
 
-    niggliReduceKernel<<<1, num_cells>>>(reinterpret_cast<const float(*)[9]>(cell.data_ptr<float>()), reinterpret_cast<float(*)[9]>(result.data_ptr<float>()), tol, num_iterations);
+    niggliReduceKernel<<<1, num_cells>>>(
+        reinterpret_cast<const float(*)[9]>(cell.data_ptr<float>()),
+        reinterpret_cast<float(*)[9]>(result.data_ptr<float>()),
+        tol,
+        num_iterations);
 
     cudaDeviceSynchronize();
 
